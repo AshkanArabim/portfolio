6ee5e3d40679d80d7ad0c5ef6ba456a8c7d8477f
AshkanArabim <ashkan.arabim@gmail.com>
2023-05-22 16:40:56 -0600

    create home component

---
+++ portfolio/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+import Hero from "../Hero/Hero";
+
+// contains anything I wnat to put in the home page
+export default function Home() {
+  return (
+    <Hero></Hero>
+  )
+}
---
+++ portfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import './Nav.css'
+
+export default function Nav() {
+	return(
+    <header>
+      <a>Home</a>
+      <a>Projects</a>
+    </header>
+  )
+}
