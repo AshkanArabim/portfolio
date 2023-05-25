cff37030d505a3314abfd067a767a0d4ed138ecc
AshkanArabim <ashkan.arabim@gmail.com>
2023-05-25 10:19:02 -0600

    separate database

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,55 +1,11 @@
 import "./Projects.css";
 import Project from "../Project/Project";
+import data from "../../data/data.json"
 
 export default function Projects() {
-	const projects_data = [
-		{
-			title: "Test Project 1",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "This is a test project that I will use to demonstrate how a project component will look.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-		{
-			title: "Test Project 2",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "Another fake one.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-		{
-			title: "Test Project 1",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "This is a test project that I will use to demonstrate how a project component will look.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-		{
-			title: "Test Project 2",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "Another fake one.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-		{
-			title: "Test Project 1",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "This is a test project that I will use to demonstrate how a project component will look.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-		{
-			title: "Test Project 2",
-      date: '4/20/1969',
-      link: 'https://google.com',
-			desc: "Another fake one.",
-      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
-		},
-	];
-
 	return (
 		<div className="projects">
-			{projects_data.map((data) => (
+			{data.projects.map((data) => (
 				<Project {...data} />
 			))}
 		</div>
