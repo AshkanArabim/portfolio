c1ec71f97b20fdcfda71eff0c61f498ee0546c2c
AshkanArabim <ashkan.arabim@gmail.com>
2023-05-25 10:08:13 -0600

    restyle project cards

--- portfolio/src/components/Project/Project.jsx
+++ portfolio/src/components/Project/Project.jsx
@@ -1,13 +1,21 @@
-import "./Project.css"
+import "./Project.css";
 
 export default function Project(props) {
+	function linkProject() {
+		// redirects to project page
+		window.location.pathname = "./" + props.title;
+	}
+
 	return (
-		<div className="project">
+		<div className="project" onClick={linkProject}>
+			<img src={props.img} alt={props.title + "project pic"} />
 			<h3>{props.title}</h3>
-			<p>{props.desc}</p>
-      <p>{props.date}</p>
-      <p>{props.link}</p>
-      <img src={props.img} alt="Project image" />
+			<p>
+				{props.desc} <br />
+				Click to read more...
+			</p>
+			{/* <p>{props.date}</p> */}
+			{/* <p>{props.link}</p> */}
 		</div>
 	);
 }
--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -17,6 +17,34 @@ export default function Projects() {
 			desc: "Another fake one.",
       img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
 		},
+		{
+			title: "Test Project 1",
+      date: '4/20/1969',
+      link: 'https://google.com',
+			desc: "This is a test project that I will use to demonstrate how a project component will look.",
+      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
+		},
+		{
+			title: "Test Project 2",
+      date: '4/20/1969',
+      link: 'https://google.com',
+			desc: "Another fake one.",
+      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
+		},
+		{
+			title: "Test Project 1",
+      date: '4/20/1969',
+      link: 'https://google.com',
+			desc: "This is a test project that I will use to demonstrate how a project component will look.",
+      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
+		},
+		{
+			title: "Test Project 2",
+      date: '4/20/1969',
+      link: 'https://google.com',
+			desc: "Another fake one.",
+      img: 'https://thumbs.dreamstime.com/b/generic-person-gray-photo-placeholder-man-silhouette-white-background-144511705.jpg'
+		},
 	];
 
 	return (
